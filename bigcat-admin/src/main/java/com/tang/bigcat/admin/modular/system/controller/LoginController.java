4e98edd2f461b3bc6e95206cb5661f2b5be4ff37
tangcaiji <tangcaiji@tangcaiji>
2019-09-07 14:28:46 +0800

    提交admin

---
+++ bigcat-admin/src/main/java/com/tang/bigcat/admin/modular/system/controller/LoginController.java
@@ -0,0 +1,37 @@
+package com.tang.bigcat.admin.modular.system.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.tang.bigcat.service.AppService;
+
+import java.util.List;
+
+/**
+ * 登录控制器
+ *
+ * @author cjtang
+ * @Date 2019-9-3
+ */
+@Controller
+public class LoginController {
+
+    @Autowired
+    AppService appService;
+
+    /**
+     * 点击登录执行的动作
+     */
+    @RequestMapping(value = "/login")
+    public String loginVali(Model model) {
+    	String app=appService.app();
+    	model.addAttribute("app", app);
+    	return "/login.jsp";
+     
+    }
+
+}
